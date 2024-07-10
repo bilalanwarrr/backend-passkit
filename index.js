687c994ffb71dbbb20f248adc7467cf4e7252483
Muhammad Qasim Ali <SP21-BCS-066@ISBSTUDENT.COMSATS.EDU.PK>
2024-07-10 16:56:24 +0500

    fix: cors origin url

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const authRoutes = require("./src/routes/authRoutes");
 const verifyToken = require("./utils/verifyToken");
 
 const corsOptions = {
-  origin: "http://localhost:5173",
+  origin: "https://backend.smartchecks.app",
   optionsSuccessStatus: 200, // Some legacy browsers (IE11, various SmartTVs) choke on 204
 };
 
