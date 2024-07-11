4af6b2795b567af5357a85d7bd5a1f69a08df6e6
Muhammad Qasim Ali <SP21-BCS-066@ISBSTUDENT.COMSATS.EDU.PK>
2024-07-11 22:54:30 +0500

    Update index.js

--- index.js
+++ index.js
@@ -29,16 +29,6 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors(corsOptions));
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-  if (req.method === "OPTIONS") {
-    res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
-    return res.status(200).json({});
-  }
-  next();
-});
-
 // Routes
 app.use("/api/passkit", passkitRoutes);
 app.use("/api/pass", passRoutes);
