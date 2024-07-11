f284afecb35a112697324a3d618b4f3e331db6cb
Muhammad Qasim Ali <SP21-BCS-066@ISBSTUDENT.COMSATS.EDU.PK>
2024-07-11 23:10:56 +0500

    Update index.js

--- index.js
+++ index.js
@@ -20,8 +20,8 @@ const PORT = process.env.PORT || 3000;
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
   if (req.method === "OPTIONS") {
-    res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
     return res.status(200).json({});
   }
   next();
