904466e69d2aab82cbce080fb4293447fac7719d
Muhammad Qasim Ali <SP21-BCS-066@ISBSTUDENT.COMSATS.EDU.PK>
2024-07-12 15:15:21 +0500

    Update index.js

--- index.js
+++ index.js
@@ -21,14 +21,22 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use((req, res, next) => {
-  console.log("origin: ",req.headers.origin)
-  res.header("Access-Control-Allow-Origin", ["https://smartchecks.app", "http://localhost:5173", "http://localhost:4173", "*"]);
+  const allowedOrigins = ["https://smartchecks.app", "http://localhost:5173", "http://localhost:4173"];
+  const origin = req.headers.origin;
+
+  if (allowedOrigins.includes(origin)) {
+    res.header("Access-Control-Allow-Origin", origin);
+  } else {
+    res.header("Access-Control-Allow-Origin", "*");
+  }
+
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-  res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
-  console.log("headers: ", res.headers)
+  res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET, OPTIONS");
+
   if (req.method === "OPTIONS") {
     return res.status(200).json({});
   }
+
   next();
 });
 
